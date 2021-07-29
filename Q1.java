4cffde870a7361453762eb01a96c8ed98c91bd5b
Muhammad Fadly <63824587+MuhammadFadly11@users.noreply.github.com>
2021-07-30 00:13:38 +0700

    Add files via upload
    Gits Q1 Qualification

---
+++ Q1.java
@@ -0,0 +1,24 @@
+public class Q1 {
+    public static void main(String args[]) {
+      int a=12;
+      int b=20;
+      int c=30;
+      System.out.println(a);
+      if(a % 3 == 0)
+      {
+        System.out.println("Hello");
+      }
+        
+        System.out.println(b);
+        if(b % 5 == 0)
+        {
+            System.out.println("World");
+        }
+        
+        System.out.println(c);
+        if(b % 5 == 0 && b % 5 == 0)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
